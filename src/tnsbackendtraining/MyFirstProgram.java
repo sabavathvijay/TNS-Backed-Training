5cb01e79880412a3adb56100b1d5177115e5682f
sabavathvijay <vijaysabavath8@gmail.com>
2025-08-04 19:39:34 +0530

    My first Commit

--- src/tnsbackendtraining/MyFirstProgram.java
+++ src/tnsbackendtraining/MyFirstProgram.java
@@ -3,7 +3,7 @@ package tnsbackendtraining;
 public class MyFirstProgram {
 
     public	static void main(String args[]) {
-    	System.out.println("Welcom to TNS Training. ");
+    	System.out.println("Welcom to TNS Training...! ");
     }
 	
 	
